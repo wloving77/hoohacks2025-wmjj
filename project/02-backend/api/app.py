4004128e4cd99503a47bf71fad4d31e1b7a301af
wloving77 <wloving77@gmail.com>
2025-03-30 11:29:23 -0400

    done again

--- project/02-backend/api/app.py
+++ project/02-backend/api/app.py
@@ -401,4 +401,4 @@ def gemini_summarize():
 # ===============================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
